c285dc6987f03032d3e417075f1d5d00f4804ab1
Emerson0607 <martinezemerson52@gmail.com>
2024-07-04 17:53:58 +0800

    three projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,20 +34,20 @@ const Projects = () => {
         sx={{ flexGrow: 1, height: '100vh' }} // Adjust height as needed
       >
         <Grid container gap={2} justifyContent="center" alignItems="center" sx={{ width: '100%', marginTop: '-10rem' }}>
-          <Grid item xs={3} sx={{ height: '490px' }}>
+          <Grid item xs={3} sx={{ height: '500px' }}>
             <Card sx={{ width: '100%', height: '100%', margin: '0 auto', boxShadow: 'none', border: '1px solid #dedede' }}>
               <CardMedia
                 component="img"
                 alt="Project Image"
                 height="40%"
-                image={process.env.PUBLIC_URL + '/image/eme.JPG'} 
+                image={process.env.PUBLIC_URL + '/image/portfolio.png'} 
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 600 }}>
-                  DSS CESU
+                  My Portfolio
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  A Decision Support System for Community Extension Services Using Machine Learning Model. The system leverages machine learning to analyze gathered data, assisting users in decision-making by predicting and recommending the top three programs for community implementation.
+                This portfolio marks my exciting journey into the world of web development, crafted entirely with React, Material UI, and Mantine components—a technology stack I'm actively mastering to build modern, responsive web applications.
                 </Typography>
               </CardContent>
               <CardActions>
@@ -66,16 +66,19 @@ const Projects = () => {
                         <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7',color:'#717171'  }}>Javascript</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7',color:'#717171'  }}>Python</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7',color:'#717171'  }}>Node.js</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171' }}>Flask</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171' }}>React</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7', color:'#717171' }}>SQLite</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7', color:'#717171' }}>Material UI</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7', color:'#717171' }}>Bootstrap</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7', color:'#717171' }}>Mantine</Item>
+                      </Grid>
+                      <Grid item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7', color:'#717171' }}>Vercel</Item>
                       </Grid>
                     
                     </Grid>
@@ -84,20 +87,20 @@ const Projects = () => {
               </CardActions>
             </Card>
           </Grid>
-          <Grid item xs={3} sx={{ height: '490px' }}>
+          <Grid item xs={3} sx={{ height: '500px' }}>
             <Card sx={{ width: '100%', height: '100%', margin: '0 auto', boxShadow: 'none', border: '1px solid #dedede' }}>
               <CardMedia
                 component="img"
                 alt="Project Image"
                 height="40%"
-                image={process.env.PUBLIC_URL + '/image/eme.JPG'} 
+                image={process.env.PUBLIC_URL + '/image/cesu.png'} 
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 600 }}>
                   DSS CESU
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  A Decision Support System for Community Extension Services Using Machine Learning Model. The system leverages machine learning to analyze gathered data, assisting users in decision-making by predicting and recommending the top three programs for community implementation.
+                  A Decision Support System for Community Extension Services Using Machine Learning Model is a web application using Flask Python framework. The system leverages machine learning to analyze gathered data, assisting users in decision-making by predicting and recommending the top three programs for community implementation.
                 </Typography>
               </CardContent>
               <CardActions>
@@ -134,20 +137,20 @@ const Projects = () => {
               </CardActions>
             </Card>
           </Grid>
-          <Grid item xs={3} sx={{ height: '490px' }}>
+          <Grid item xs={3} sx={{ height: '500px' }}>
             <Card sx={{ width: '100%', height: '100%', margin: '0 auto', boxShadow: 'none', border: '1px solid #dedede' }}>
               <CardMedia
                 component="img"
                 alt="Project Image"
                 height="40%"
-                image={process.env.PUBLIC_URL + '/image/eme.JPG'} 
+                image={process.env.PUBLIC_URL + '/image/jsis.png'} 
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 600 }}>
-                  DSS CESU
+                  Juan's Sales and Inventory Management System
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  A Decision Support System for Community Extension Services Using Machine Learning Model. The system leverages machine learning to analyze gathered data, assisting users in decision-making by predicting and recommending the top three programs for community implementation.
+                Developed for a client to streamline operations in their food business. This project showcases my expertise in building robust desktop applications using Vb.NET and integrating backend functionalities to ensure smooth business operations.
                 </Typography>
               </CardContent>
               <CardActions>
@@ -157,26 +160,18 @@ const Projects = () => {
                     <Grid container spacing={1} sx={{ width: '100%'}} >
                         
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171'  }}>HTML</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171'  }}>Visual Basic</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171'  }}>CSS</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171'  }}>.NET Framework</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171'  }}>Javascript</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px', backgroundColor: '#D9E3F7',color:'#717171'  }}>GUNA Framework</Item>
                       </Grid>
                       <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7',color:'#717171'  }}>Python</Item>
+                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7',color:'#717171'  }}>MySQL</Item>
                       </Grid>
-                      <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7',color:'#717171' }}>Flask</Item>
-                      </Grid>
-                      <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7', color:'#717171' }}>SQLite</Item>
-                      </Grid>
-                      <Grid item>
-                        <Item sx={{ fontSize: '12px', fontWeight: 500, padding: '5px',  backgroundColor: '#D9E3F7', color:'#717171' }}>Bootstrap</Item>
-                      </Grid>
+             
                     </Grid>
                   </Item>
                 </Grid>
